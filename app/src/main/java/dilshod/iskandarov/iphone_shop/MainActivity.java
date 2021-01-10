e2bf2ebadd845ce0d2975c7f2e693d3d36554857
DilshodIskandarov456 <76392416+DilshodIskandarov456@users.noreply.github.com>
2021-01-10 23:53:27 +0300

    new

--- app/src/main/java/dilshod/iskandarov/iphone_shop/MainActivity.java
+++ app/src/main/java/dilshod/iskandarov/iphone_shop/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setAdapter(arrayAdapter);
     }
 
+
     void oncreateHashMap(){
         goodsMap = new HashMap();
         goodsMap.put("iPhone", 300);
